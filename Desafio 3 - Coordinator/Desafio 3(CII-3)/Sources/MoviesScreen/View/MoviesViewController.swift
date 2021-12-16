2dd2235d7bbab96687a1478934b145a0a3909618
Guilherme Daniel Fernandes da Silva <guilhermedfs11@gmail.com>
2021-12-16 18:28:57 -0300

    some design changes

--- Desafio 3 - Coordinator/Desafio 3(CII-3)/Sources/MoviesScreen/View/MoviesViewController.swift
+++ Desafio 3 - Coordinator/Desafio 3(CII-3)/Sources/MoviesScreen/View/MoviesViewController.swift
@@ -69,7 +69,7 @@ extension MoviesViewController: UICollectionViewDataSource {
 
 extension MoviesViewController: UICollectionViewDelegateFlowLayout{
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 180, height: 300)
+        return CGSize(width: 205, height: 300)
     }
 }
 
@@ -86,6 +86,8 @@ extension MoviesViewController: UICollectionViewDelegate {
             moviesViewModel.loadPages()
         }
     }*/
+    
+    
     
 }
 
